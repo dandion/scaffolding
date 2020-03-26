60df2ba6fdb1c0e643832757467602eda4d7a0e7
Daniel Andion <dan.and.gar@gmail.com>
2020-03-26 13:00:22 +1100

    bash requires

--- bash/script.bash
+++ bash/script.bash
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+readonly required=( top  missing )
+
 function info () {
     echo -e "[\e[34;1mINFO\e[0m] $@"
 }
 
 function error () {
-    echo -e "[\e[31;1mFAIL\e[0m] $@"
+    echo -e "[\e[31;1mERROR\e[0m] $@"
 }
 
 function success () {
@@ -17,11 +19,21 @@ function failure () {
     echo -e "[\e[31;1mFAIL\e[0m] $@"
 }
 
+function check_required() {
+    for cmd in "${required[@]}"; do
+        command -v ${cmd} &>/dev/null \
+            || ( error "command '${cmd}' not found: \
+This script requires: ${required[@]}" \
+                     && exit 1 )
+    done
+}
 
 function main () {
 
-    # ...
+    info '...'
     
 }
+
+check_required
 
 main "$@"
