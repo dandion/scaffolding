cc3feb738c55bc37cb43aa9dfcdfcbbbc4826d8d
Daniel Andion <dan.and.gar@gmail.com>
2022-11-23 14:18:26 +1100

    2-space indent

--- bash/script.bash
+++ bash/script.bash
@@ -10,37 +10,37 @@ set -euo pipefail
 readonly requires=( source bind ) 
 
 function info () {
-    echo -e "[\e[34;1mINFO\e[0m] $@"
+  echo -e "[\e[34;1mINFO\e[0m] $@"
 }
 
 function error () {
-    echo -e "[\e[31;1mERROR\e[0m] $@"
+  echo -e "[\e[31;1mERROR\e[0m] $@"
 }
 
 function success () {
-    echo -e "[\e[32;1mPASS\e[0m] $@"
+  echo -e "[\e[32;1mPASS\e[0m] $@"
 }
 
 function failure () {
-    echo -e "[\e[31;1mFAIL\e[0m] $@"
+  echo -e "[\e[31;1mFAIL\e[0m] $@"
 }
 
 function timestamp () {
-   date "+%S%m%d%H%M%Y"
+  date "+%S%m%d%H%M%Y"
 }
 
 function check_required() {
-    for cmd in "${requires[@]}"; do
-        command -v ${cmd} &>/dev/null \
-            || ( error "command '${cmd}' not found: \
+  for cmd in "${requires[@]}"; do
+    command -v ${cmd} &>/dev/null \
+      || ( error "command '${cmd}' not found: \
 This script requires: ${requires[@]}" \
-                     && exit 1 )
-    done
+        && exit 1 )
+  done
 }
 
 function main () {
 
-    info '...'
+  info '...'
     
 }
 
